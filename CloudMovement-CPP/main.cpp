bc6435c5094c77e44d49f8a022952472f7f45d0b
Marius E. Penteliuc <marius@penteliuc.com>
2020-11-11 15:18:38 +0200

    Initial Commit

---
+++ CloudMovement-CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CloudMovement-CPP
+//
+//  Created by Marius E. Penteliuc on 11.11.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
