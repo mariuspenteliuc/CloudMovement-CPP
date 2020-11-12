091c8344dad924299d198c706b72a2d27d6824ff
Marius E. Penteliuc <marius@penteliuc.com>
2020-11-12 14:20:10 +0200

    Output OpenCV version to console

--- CloudMovement-CPP/main.cpp
+++ CloudMovement-CPP/main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+#include "OpticalFlowService.hpp"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "OpenCV version: " << OpticalFlowService::getOpenCVVersion() << endl;
+
     return 0;
 }
+
+
+
+//    Mat img = imread("/Users/mariuspenteliuc/Desktop/image.png"); //Change the image path here.
+//        if (img.data == 0) {
+//            cerr << "Image not found!" << endl;
+//            return -1;
+//        }
+//    namedWindow("image", WINDOW_AUTOSIZE);
+//        imshow("image", img);
+//        waitKey();
