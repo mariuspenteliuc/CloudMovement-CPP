ef59663d9958dfb396601775c89a98dc1a2e36de
Marius E. Penteliuc <marius@penteliuc.com>
2020-11-12 14:19:41 +0200

    Created class to perform Optical Flow operations

---
+++ CloudMovement-CPP/OpticalFlowService.hpp
@@ -0,0 +1,22 @@
+//
+//  OpticalFlowService.hpp
+//  CloudMovement-CPP
+//
+//  Created by Marius E. Penteliuc on 12.11.2020.
+//
+
+#ifndef OpticalFlowService_hpp
+#define OpticalFlowService_hpp
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class OpticalFlowService {
+
+public:
+    static string getOpenCVVersion();
+};
+
+#endif /* OpticalFlowService_hpp */
