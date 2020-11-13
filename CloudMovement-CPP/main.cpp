b2c8c728f8f887785fae5a545fde8be75edc935e
Marius E. Penteliuc <marius@penteliuc.com>
2020-11-13 14:18:06 +0200

    included more headers and declared static global variables for input and output folders

--- CloudMovement-CPP/main.cpp
+++ CloudMovement-CPP/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include "OpticalFlowService.hpp"
+#include "ImageHelper.hpp"
+#include <cstdlib>
+#include <ctime>
+#include "Scene.hpp"
 
 using namespace std;
+using namespace cv;
+
+const static string DEBUG_IN = "/Users/mariuspenteliuc/Assets/PhD/debug/debug_in";
+const static string DEBUG_OUT = "/Users/mariuspenteliuc/Assets/PhD/debug/debug_out";
 
 int main(int argc, const char * argv[]) {
     std::cout << "OpenCV version: " << OpticalFlowService::getOpenCVVersion() << endl;
