f47118756c43ed809294c228f06e0dd38cb430c3
Marius E. Penteliuc <marius@penteliuc.com>
2020-11-17 12:15:19 +0200

    I didn't like it when I saw that the opening curly brace for most functions here where right next to the closing parenthesis. I couldn't believe my eyes that I wrote it that way. No more! I fixed it!
    Does anyone read this? You reading this, are you enjoying my commit messages?

--- source/Helpers/MathHelper.cpp
+++ source/Helpers/MathHelper.cpp
@@ -7,22 +7,22 @@
 
 #include "MathHelper.hpp"
 
-Point2f addPoints(Point2f v1, Point2f v2){
+Point2f addPoints(Point2f v1, Point2f v2) {
     float x = v1.x + v2.x;
     float y = v1.y + v2.y;
     return Point2f(x, y);
 }
-Point2f removePoints(Point2f v1, Point2f v2){
+Point2f removePoints(Point2f v1, Point2f v2) {
     float x = v1.x - v2.x;
     float y = v1.y - v2.y;
     return Point2f(x, y);
 }
-Point2f removePoints(Point2f v1, float a){
+Point2f removePoints(Point2f v1, float a) {
     float x = v1.x - a;
     float y = v1.y - a;
     return Point2f(x, y);
 }
-Point2f dividePoints(Point2f v1, float a){
+Point2f dividePoints(Point2f v1, float a) {
     float x = v1.x / a;
     float y = v1.y / a;
     return Point2f(x, y);
