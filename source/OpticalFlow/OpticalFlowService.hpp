95b0d65e114d99a4fd6949f62e58429d0b00cd40
Marius E. Penteliuc <marius@penteliuc.com>
2021-01-27 09:22:45 +0200

    made overlayFlowLines() function public

--- source/OpticalFlow/OpticalFlowService.hpp
+++ source/OpticalFlow/OpticalFlowService.hpp
@@ -21,8 +21,8 @@ class OpticalFlowService {
 private:
     static void drawOpticalFlowMap(const cv::Mat& flow, cv::Mat& cflowmap, int step, double, const cv::Scalar& color);
     static cv::Mat getOpticalFlowFarneback(cv::Mat firstImage, cv::Mat secondImage);
-    static cv::Mat overlayFlowLines(cv::Mat flow, cv::Mat image);
 public:
+    static cv::Mat overlayFlowLines(cv::Mat flow, cv::Mat image);
     OpticalFlowService();
     static int computeFlowForImages(string inputPath, string outputPath, string fileType, bool saveOverlays, bool saveFlows, bool previewOverlays);
     static cv::Mat averageFlows(string inputPath);
