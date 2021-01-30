2739c28428cef8e4eaa96933674059cd04ab9c17
Marius E. Penteliuc <marius@penteliuc.com>
2021-01-30 18:09:10 +0200

    updated drawScene() to use the outputFolder property

--- source/Boids/Scene.cpp
+++ source/Boids/Scene.cpp
@@ -328,7 +328,7 @@ void Scene::drawScene() {
         circle(scene, point, .5, cv::Scalar(255, 255, 255, 0), cv::FILLED);
     }
 //    FileHelper::writeFile("/Users/mariuspenteliuc/Assets/PhD/debug/debug_out/boids/boids_" + std::string(5 - to_string(framesSaved).length(), '0') + std::to_string(framesSaved) + ".jpg", scene);
-    FileHelper::writeFile("/Volumes/Transfers/Experiments/day_window_1/test_1/simulated_clouds/boids_" + std::string(5 - to_string(framesSaved).length(), '0') + std::to_string(framesSaved) + ".jpg", scene);
+    FileHelper::writeFile(outputFolder + "/simulated_clouds/boids_" + std::string(5 - to_string(framesSaved).length(), '0') + std::to_string(framesSaved) + ".jpg", scene);
     framesSaved++;
     imshow("OpticalFlow", scene);
     if (Scene::previewSimulation) {
